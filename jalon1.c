4d15b68d72c9757e4cd1bc4307e34093493ab4c0
Guillaume Landré <guillaumelandre101@gmail.com>
2018-10-25 10:48:41 +0100

    initial push

---
+++ jalon1.c
@@ -0,0 +1,15 @@
+/*
+* @Author: GuillaumeLandre
+* @Date:   2018-10-25 10:38:45
+* @Last Modified by:   GuillaumeLandre
+* @Last Modified time: 2018-10-25 10:38:45
+*/
+#include <stdio.h>
+
+ int main(void)
+ {
+ 	int var = 10;
+
+ 	printf("valeur de var %d\n", var);
+ 	return 0;
+ }
