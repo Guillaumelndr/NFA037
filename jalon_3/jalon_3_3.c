f33ee22d26069c5c51ca740d8baa965a51409e46
Guillaume Landré <guillaumelandre101@gmail.com>
2018-11-07 13:28:56 +0000

    end jalon_3

---
+++ jalon_3/jalon_3_3.c
@@ -0,0 +1,18 @@
+/**
+ * @Author: GuillaumeLandre
+ * @Date:   2018-10-26T14:41:10+01:00
+ * @Last modified by:   GuillaumeLandre
+ * @Last modified time: 2018-11-07T13:18:09+00:00
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  int a = "0x2A";
+  int b = 0;
+
+  printf("valeur de a en hexa = %x\n", a);
+  return (0);
+}
